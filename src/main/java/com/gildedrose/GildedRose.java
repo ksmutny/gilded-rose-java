b6efa4206ef3b916f26cf80809a336c7334a4573
Karel Smutný <karel.smutny@outlook.com>
2024-01-08 18:36:32 +0100

    Split Aged Brie and Backstage passes branch to separate branches

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,20 +9,20 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals("Aged Brie") || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
+            if (item.name.equals("Aged Brie")) {
+                if (item.quality < 50)
+                    item.quality++;
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality < 50)
                     item.quality++;
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11)
-                            if (item.quality < 50)
-                                item.quality++;
+                if (item.sellIn < 11)
+                    if (item.quality < 50)
+                        item.quality++;
 
-                        if (item.sellIn < 6)
-                            if (item.quality < 50)
-                                item.quality++;
-                    }
-                }
+                if (item.sellIn < 6)
+                    if (item.quality < 50)
+                        item.quality++;
             } else {
                 if (item.quality > 0)
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros"))

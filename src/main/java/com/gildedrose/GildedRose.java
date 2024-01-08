37051017c9d57a04adad00e0bebe35eb1b1d1e15
Karel Smutný <karel.smutny@outlook.com>
2024-01-08 19:44:19 +0100

    Introduced standardAging()

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -21,17 +21,21 @@ class GildedRose {
     private int qualityAdjustment(Item item) {
         switch (item.name) {
             case "Aged Brie":
-                return item.sellIn <= 0 ? 2 : 1;
+                return -1 * standardAging(item);
             case "Backstage passes to a TAFKAL80ETC concert":
                 if (item.sellIn <= 0) return -item.quality;
                 else if (item.sellIn <= 5) return 3;
                 else if (item.sellIn <= 10) return 2;
                 else return 1;
             case "Conjured Mana Cake":
-                return item.sellIn <= 0 ? -4 : -2;
+                return 2 * standardAging(item);
             default:
-                return item.sellIn <= 0 ? -2 : -1;
+                return standardAging(item);
         }
+    }
+
+    private int standardAging(Item item) {
+        return item.sellIn > 0 ? -1 : -2;
     }
 
     private int clamp(int value, int min, int max) {

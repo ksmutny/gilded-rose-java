b3cb0aee8c6938937cd3c35aeb43d0c4933bb119
Karel Smutný <karel.smutny@outlook.com>
2024-01-08 19:08:10 +0100

    Moved all Sulfuras never-change logic to a single place

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,6 +9,8 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")) continue;
+
             if (item.name.equals("Aged Brie")) {
                 incQualityUpTo50(item);
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -22,22 +24,18 @@ class GildedRose {
                     adjustment = 1;
 
                 incQualityUpTo50(item, adjustment);
-            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-
             } else {
                 if (item.quality > 0)
                     item.quality--;
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
-                item.sellIn--;
+            item.sellIn--;
 
             if (item.sellIn < 0)
                 if (!item.name.equals("Aged Brie"))
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.quality > 0)
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
-                                item.quality--;
+                            item.quality--;
                     } else {
                         item.quality = 0;
                     }
